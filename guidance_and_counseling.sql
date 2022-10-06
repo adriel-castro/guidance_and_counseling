444335bbd8665159ba83bfcd82c76c91cec5ffb4
adriel-dev <adriel27castro@gmail.com>
2022-10-07 00:42:44 +0800

    update database

--- guidance_and_counseling.sql
+++ guidance_and_counseling.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2022 at 05:28 PM
+-- Generation Time: Oct 06, 2022 at 06:40 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -60,7 +60,7 @@ CREATE TABLE `refferals` (
 --
 
 INSERT INTO `refferals` (`ref_id`, `reffered_user`, `user`, `source`, `reffered_by`, `reffered_date`, `nature`, `reason`, `actions`, `remarks`, `ref_status`, `updated_at`) VALUES
-(1, 6, 3, 'Parent/Guardian', 'Classmate', '2022-10-04', 'Academic', 'Bullying', 'Teachers counseling', 'Need Psychiatry', 'Pending', '2022-10-06 14:03:29'),
+(1, 6, 3, 'Parent/Guardian', 'Classmate', '2022-10-04', 'Academic', 'Bullying', 'Teachers counseling', 'Need Psychiatry', 'Pending', '2022-10-06 16:40:01'),
 (2, 3, 3, 'Faculty', 'Classmate', '2022-10-04', 'Career', 'Madaling mainis', 'Have Tutor', 'Unhealthy Environment', 'Pending', '2022-10-06 13:44:31'),
 (3, 7, 2, 'Faculty', 'Parent/Guardian', '2022-10-03', 'Personal', 'Nagwawala', 'Pinacheck up sa Doctor', 'Needs Psychiatry', 'For Approval', '2022-10-06 15:27:57'),
 (5, 5, 3, 'Guidance Counselor', 'Parent', '2022-09-26', 'Personal', 'Slow learner', 'Kinausap ng teacher ng masisinsinan', 'Unhealthy Environment', 'Completed', '2022-10-06 13:13:41'),
