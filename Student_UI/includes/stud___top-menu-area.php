a734220949b46fbc9af1eced25e600f287566bb2
adriel-dev <adriel27castro@gmail.com>
2022-10-06 22:45:03 +0800

    Update stud___top-menu-area.php

--- Student_UI/includes/stud___top-menu-area.php
+++ Student_UI/includes/stud___top-menu-area.php
@@ -1,3 +1,7 @@
+<?php
+    // session_start();
+?>
+
 
 <!-- Start Welcome area -->
     <div class="all-content-wrapper">
@@ -174,10 +178,11 @@
                                                         <li><a href="#"><span class="edu-icon edu-settings author-log-ic"></span>Settings</a>
                                                         </li>
                                                         <li>
-                                                            <!-- <a href="#"><span class="edu-icon edu-locked author-log-ic"></span>Log Out</a> -->
-                                                            <a class="dropdown-item" href="../homepage___index.php" data-toggle="modal" >
-                                                            Logout
+                                                        <?php if(isset($_SESSION['UserEmail'])) { ?>
+                                                            <a class="dropdown-item" href="../logout.php" data-toggle="modal" >
+                                                                Logout
                                                             </a>
+                                                        <?php } ?>
                                                         </li>
                                                     </ul>
                                                 </li>
