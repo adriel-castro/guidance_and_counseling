5eb4f32cdfeae6a9c99c33572c82168316c5daaf
adriel-dev <adriel27castro@gmail.com>
2022-10-12 23:52:50 +0800

    Update gc___dashboard.php

--- Guidance_Counselor_UI/gc___dashboard.php
+++ Guidance_Counselor_UI/gc___dashboard.php
@@ -1,4 +1,15 @@
 <?php
+session_start();
+
+include_once("../connections/connection.php");
+
+if(!isset($_SESSION['UserEmail'])){
+        
+    echo "<script>window.open('../homepage___login.php','_self')</script>";
+    
+}else{
+
+$con = connection();
 
 function build_calendar($month, $year)
 {
@@ -168,7 +179,6 @@ include('includes/gc___mobile_menu.php');
                     <div class="row">
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                             <div class="breadcome-heading">
-                                <!-- this is for the search bar that has been removed since this is a dashboard-->
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
@@ -187,39 +197,6 @@ include('includes/gc___mobile_menu.php');
 </div>
 </div>
 
-<!-- <div class="modal-bootstrap shadow-inner res-mg-b-30">
-    <h2>Header BG Color Modal</h2>
-    <p>Bootstrap provides an easy way to create predefined alert messages. They support a number of use cases from user notification to completely custom content and feature a handful of helpful subcomponents, sizes, and more.</p>
-    <div class="modal-area-button">
-        <a class="Primary mg-b-10" href="#" data-toggle="modal" data-target="#PrimaryModalhdbgcl">Primary</a>
-        <a class="Information Information-color mg-b-10" href="#" data-toggle="modal" data-target="#InformationproModalhdbgcl">Information</a>
-        <a class="Warning Warning-color mg-b-10" href="#" data-toggle="modal" data-target="#WarningModalhdbgcl">Warning</a>
-        <a class="Danger danger-color" href="#" data-toggle="modal" data-target="#DangerModalhdbgcl">Danger</a>
-    </div>
-</div>
-<div id="PrimaryModalhdbgcl" class="modal modal-edu-general default-popup-PrimaryModal fade" role="dialog">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header header-color-modal bg-color-1">
-                <h4 class="modal-title">BG Color Header Modal</h4>
-                <div class="modal-close-area modal-close-df">
-                    <a class="close" data-dismiss="modal" href="#"><i class="fa fa-close"></i></a>
-                </div>
-            </div>
-            <div class="modal-body">
-                <i class="educate-icon educate-checked modal-check-pro"></i>
-                <h2>Awesome!</h2>
-                <p>The Modal plugin is a dialog box/popup window that is displayed on top of the current page</p>
-            </div>
-            <div class="modal-footer">
-                <a data-dismiss="modal" href="#">Cancel</a>
-                <a href="#">Process</a>
-            </div>
-        </div>
-    </div>
-</div> -->
-
-
 <div class="analytics-sparkle-area">
     <div class="container-fluid">
         <div class="row">
@@ -227,19 +204,14 @@ include('includes/gc___mobile_menu.php');
                 <div class="analytics-sparkle-line reso-mg-b-30">
                     <div class="analytics-content">
                         <h5>Students</h5>
-                        <!-- this if for the printing of total reg tertiary in the dashboard.php -->
+
                         <?php
-                        require 'db_config.php';
-
-                        $query = " SELECT STUD_ID FROM student_tbl ORDER BY STUD_ID ";
-                        $query_run = mysqli_query($connection, $query);
-
-                        $row = mysqli_num_rows($query_run);
-
-                        echo '<h2><span class="counter">' . $row . '</span>
+                            $query = "SELECT id_number FROM users WHERE position = 'student' || 'Student' ORDER BY id_number";
+                            $query_run = mysqli_query($con, $query);
+                            $row = mysqli_num_rows($query_run);
+                            echo '<h2><span class="counter">' . $row . '</span>
                             <span class="tuition-fees">Students</span></h2>'
                         ?>
-
 
                     </div>
                 </div>
@@ -249,17 +221,12 @@ include('includes/gc___mobile_menu.php');
                 <div class="analytics-sparkle-line reso-mg-b-30 table-mg-t-pro dk-res-t-pro-30">
                     <div class="analytics-content">
                         <h5>Staff</h5>
-                        <!-- this if for the printing of total reg tertiary in the dashboard.php -->
+                        
                         <?php
-                        require
-                            'db_config.php';
-
-                        $query = " SELECT STAFF_ID FROM staff_tbl ORDER BY STAFF_ID ";
-                        $query_run = mysqli_query($connection, $query);
-
-                        $row = mysqli_num_rows($query_run);
-
-                        echo '<h2><span class="counter">' . $row . '</span>
+                            $query = "SELECT id_number FROM users WHERE position = 'staff' || 'Staff' ORDER BY id_number";
+                            $query_run = mysqli_query($con, $query);
+                            $row = mysqli_num_rows($query_run);
+                            echo '<h2><span class="counter">' . $row . '</span>
                             <span class="tuition-fees">Staff</span></h2>'
                         ?>
 
@@ -271,18 +238,15 @@ include('includes/gc___mobile_menu.php');
                 <div class="analytics-sparkle-line table-mg-t-pro dk-res-t-pro-30">
                     <div class="analytics-content">
                         <h5>Guidance Counselor</h5>
-                        <!-- this if for the printing of total reg tertiary in the dashboard.php -->
+                        
                         <?php
-                        require 'db_config.php';
-
-                        $query = " SELECT GC_ID FROM gc_tbl ORDER BY GC_ID ";
-                        $query_run = mysqli_query($connection, $query);
-
-                        $row = mysqli_num_rows($query_run);
-
-                        echo '<h2><span class="counter">' . $row . '</span>
+                            $query = "SELECT id_number FROM users WHERE position = 'guidance' || 'Guidance' ORDER BY id_number";
+                            $query_run = mysqli_query($con, $query);
+                            $row = mysqli_num_rows($query_run);
+                            echo '<h2><span class="counter">' . $row . '</span>
                             <span class="tuition-fees">Guidance Counselor</span></h2>'
                         ?>
+
                     </div>
                 </div>
             </div>
@@ -291,167 +255,57 @@ include('includes/gc___mobile_menu.php');
                 <div class="analytics-sparkle-line reso-mg-b-30">
                     <div class="analytics-content">
                         <h5>Number of Appointments</h5>
-                        <!-- this if for the printing of total reg tertiary in the dashboard.php -->
+                        
                         <?php
-                        require 'db_config.php';
+                            $query = "SELECT id FROM appointments ORDER BY id";
+                            $query_run = mysqli_query($con, $query);
+                            $row = mysqli_num_rows($query_run);
 
-                        $query = " SELECT APPOINTMENT_ID FROM appointment_tbl ORDER BY APPOINTMENT_ID ";
-                        $query_run = mysqli_query($connection, $query);
-
-                        $row = mysqli_num_rows($query_run);
-
-                        echo '<h2><span class="counter">' . $row . '</span>
+                            echo '<h2><span class="counter">' . $row . '</span>
                             <span class="tuition-fees">Appointments</span></h2>'
                         ?>
 
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </div>
 
 
 
-<div class="product-sales-area mg-tb-30">
-    <div class="container-fluid">
-        <div class="row">
-
-
-            <div class="calender-area mg-b-15">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="calender-inner">
-                                <?php
-                                $dateComponents = getdate();
-                                if (isset($_GET['month']) && isset($_GET['year'])) {
-                                    $month = $_GET['month'];
-                                    $year = $_GET['year'];
-                                } else {
-                                    $month = $dateComponents['mon'];
-                                    $year = $dateComponents['year'];
-                                }
-                                echo build_calendar($month, $year);
-                                ?>
+    <div class="product-sales-area mg-tb-30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="calender-area mg-b-15">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="calender-inner">
+                                    <?php
+                                    $dateComponents = getdate();
+                                    if (isset($_GET['month']) && isset($_GET['year'])) {
+                                        $month = $_GET['month'];
+                                        $year = $_GET['year'];
+                                    } else {
+                                        $month = $dateComponents['mon'];
+                                        $year = $dateComponents['year'];
+                                    }
+                                    echo build_calendar($month, $year);
+                                    ?>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-            <!-- <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <div class="white-box analytics-info-cs mg-b-10 res-mg-b-30 res-mg-t-30 table-mg-t-pro-n tb-sm-res-d-n dk-res-t-d-n">
-                    <h3 class="box-title">Total Visit</h3>
-                    <ul class="list-inline two-part-sp">
-                        <li>
-                            <div id="sparklinedash"></div>
-                        </li>
-                        <li class="text-right sp-cn-r"><i class="fa fa-level-up" aria-hidden="true"></i> <span class="counter text-success">950</span></li>
-                    </ul>
-                </div>
-                <div class="white-box analytics-info-cs mg-b-10 res-mg-b-30 tb-sm-res-d-n dk-res-t-d-n">
-                    <h3 class="box-title">Page Views</h3>
-                    <ul class="list-inline two-part-sp">
-                        <li>
-                            <div id="sparklinedash2"></div>
-                        </li>
-                        <li class="text-right graph-two-ctn"><i class="fa fa-level-up" aria-hidden="true"></i> <span class="counter text-purple">913</span></li>
-                    </ul>
-                </div>
-                <div class="white-box analytics-info-cs mg-b-10 res-mg-b-30 tb-sm-res-d-n dk-res-t-d-n">
-                    <h3 class="box-title">Tele-Counseling</h3>
-                    <ul class="list-inline two-part-sp">
-                        <li>
-                            <div id="sparklinedash3"></div>
-                        </li>
-                        <li class="text-right graph-three-ctn"><i class="fa fa-level-up" aria-hidden="true"></i> <span class="counter text-info">570</span></li>
-                    </ul>
-                </div>
-                <div class="white-box analytics-info-cs table-dis-n-pro tb-sm-res-d-n dk-res-t-d-n">
-                    <h3 class="box-title">Walk-in</h3>
-                    <ul class="list-inline two-part-sp">
-                        <li>
-                            <div id="sparklinedash4"></div>
-                        </li>
-                        <li class="text-right graph-four-ctn"><i class="fa fa-level-down" aria-hidden="true"></i> <span class="text-danger"><span class="counter">50</span>%</span>
-                        </li>
-                    </ul>
-                </div>
-
-
-            </div> -->
         </div>
-
-
-
-
-        <!-- <div class="product-sales-area mg-tb-30">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
-                <div class="white-box res-mg-t-30 table-mg-t-pro-n">
-                    <h3 class="box-title">Services avail by students</h3>
-                    <ul class="country-state">
-                        <li>
-                            <h2><span class="counter">560</span></h2> <small>Information Service</small>
-                            <div class="pull-right">70% <i class="fa fa-level-up text-danger ctn-ic-1"></i></div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-danger ctn-vs-1" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:75%;"> <span class="sr-only">70% Complete</span></div>
-                            </div>
-                        </li>
-                        <li>
-                            <h2><span class="counter">280</span></h2> <small>Career Services</small>
-                            <div class="pull-right">35% <i class="fa fa-level-up text-success ctn-ic-2"></i></div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-info ctn-vs-2" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:48%;"> <span class="sr-only">35% Complete</span></div>
-                            </div>
-                        </li>
-                        <li>
-                            <h2><span class="counter">440</span></h2> <small>Guidance Services</small>
-                            <div class="pull-right">55% <i class="fa fa-level-up text-success ctn-ic-3"></i></div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-success ctn-vs-3" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:55%;"> <span class="sr-only">55% Complete</span></div>
-                            </div>
-                        </li>
-                        <li>
-                            <h2><span class="counter">264</span></h2> <small>Individual inventory Services</small>
-                            <div class="pull-right">33% <i class="fa fa-level-down text-success ctn-ic-4"></i></div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-success ctn-vs-4" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:33%;"> <span class="sr-only">33% Complete</span></div>
-                            </div>
-                        </li>
-                        <li>
-                            <h2><span class="counter">480</span></h2> <small>Counseling Services</small>
-                            <div class="pull-right">60% <i class="fa fa-level-up text-success ctn-ic-5"></i></div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-inverse ctn-vs-5" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:60%;"> <span class="sr-only">60% Complete</span></div>
-                            </div>
-                        </li>
-                        <li>
-                            <h2><span class="counter">224</span></h2> <small>Referral Services</small>
-                            <div class="pull-right">28% <i class="fa fa-level-up text-success ctn-ic-5"></i></div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-inverse ctn-vs-5" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:60%;"> <span class="sr-only">28% Complete</span></div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div> -->
-
-
-
-
-
-
-
-
     </div>
-</div>
 </div>
 
 
 <?php
 include('includes/gc___scripts.php');
 ?>
+
+<?php } ?>
