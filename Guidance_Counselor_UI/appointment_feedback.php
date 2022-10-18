cf729aa2ce0e1a8ddc502f1dfd8e73faaa05d3f6
adriel-dev <adriel27castro@gmail.com>
2022-10-18 21:36:38 +0800

    Update appointment_feedback.php

--- Guidance_Counselor_UI/appointment_feedback.php
+++ Guidance_Counselor_UI/appointment_feedback.php
@@ -24,12 +24,38 @@ if(!isset($_SESSION['UserEmail'])){
     $action_taken = $_POST['action_taken'];
     $remarks = $_POST['remarks'];
     $feedback_date = date("Y-m-d");
+    $update_app_status = "Completed";
 
     $query = "INSERT INTO `feedback`(`student_name`, `program`, `section`, `app_id`, `session_date`, `feedback_date`, `action_taken`, `remarks`) ".
             "VALUES ('$stud_name','$program','$section','$app_id','$session_date','$feedback_date','$action_taken','$remarks')";
-    $con->query($query) or die ($con->error);
-    header("Location: gc___all_appointment.php");
-  
-}
+    $fb_row = $con->query($query) or die ($con->error);
 
+    if($fb_row > 0) {
+      // Update appointment_history Status to Completed
+      $history_query = "UPDATE `appointment_history` SET `status` = '$update_app_status' WHERE app_id = '$app_id'";
+      $history_row = $con->query($history_query) or die ($con->error);
+    }
+
+    if($fb_row > 0) {
+      // Update appointments Status to Completed
+      $app_status_query = "UPDATE `appointments` SET `app_status` = '$update_app_status' WHERE id = $app_id";
+      $app_status_con = $con->query($app_status_query) or die ($con->error);
+    }
+
+    if($app_status_con > 0) {
+      // Update referrals status with an appointment
+      $app_query = "SELECT ref_id FROM appointments WHERE id = $app_id ";
+      $app_con = $con->query($app_query) or die ($con->error);
+      $app_row = $app_con->fetch_assoc();
+      
+      $ref_id = $app_row['ref_id'];
+
+      $ref_query = "UPDATE refferals SET ref_status = '$update_app_status' WHERE ref_id = '$ref_id'";
+      $con->query($ref_query) or die ($con->error);
+      header("Location: gc___all_appointment.php");
+    } 
+
+  } else {
+    header("Location: 404.php");
+  }
 }
