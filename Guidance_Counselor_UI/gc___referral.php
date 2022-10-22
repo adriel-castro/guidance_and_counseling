fb623dd84b2ecdd3a76c3ddbec19754134291abd
abby <nazab0121@gmail.com>
2022-10-23 02:36:12 +0800

    deleting the commented student id in the gc_referral form

--- Guidance_Counselor_UI/gc___referral.php
+++ Guidance_Counselor_UI/gc___referral.php
@@ -201,7 +201,6 @@ if (!isset($_SESSION['UserEmail'])) {
                                     <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
                                         <input type="text" name="student_id" class="form-control" placeholder="Enter Student ID" required/>
                                     </div>
-                                </div>
                             </div> -->
                                 <div class="form-group-inner">
                                     <div class="row">
