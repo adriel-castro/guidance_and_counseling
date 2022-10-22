f00e6570eb1658704d9ea92f87c97b58af8a31b6
abby <nazab0121@gmail.com>
2022-10-23 02:56:55 +0800

    spacing of code

--- Guidance_Counselor_UI/gc___referral.php
+++ Guidance_Counselor_UI/gc___referral.php
@@ -201,6 +201,7 @@ if (!isset($_SESSION['UserEmail'])) {
                                     <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
                                         <input type="text" name="student_id" class="form-control" placeholder="Enter Student ID" required/>
                                     </div>
+                                </div>
                             </div> -->
                                 <div class="form-group-inner">
                                     <div class="row">
