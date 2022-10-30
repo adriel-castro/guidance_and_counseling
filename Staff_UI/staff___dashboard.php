00efb3d30c8452ddf52f0be7b535fb04d7dafdd7
abby <nazab0121@gmail.com>
2022-10-30 20:56:00 +0800

    removing the offense monitoring in staff dashboard
    wala naman pong offense monitorinf si staff

--- Staff_UI/staff___dashboard.php
+++ Staff_UI/staff___dashboard.php
@@ -32,40 +32,19 @@ include('includes/staff___mobile_menu.php');
 </div>
 </div>
 
-           <div class="product-sales-area mg-tb-30">
-           <div class="container-fluid">
+<div class="col-lg-12">
+    <div class="product-sales-chart">
+        <div class="portlet-title">
             <div class="row">
+                <div class="col-lg-12">
+                    <div id='calendar'></div>
 
-            <div class="col-lg-9 col-md-12 col-sm-12 col-xs-12">
-                <div class="product-sales-chart">
-                    <div class="portlet-title">
-                        <div class="row">
-                            <div class="col-lg-12">
-                            <div id='calendar'></div>
-                                            
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
-
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <div class="white-box">
-                    <h3 class="box-title">Offense Monitoring</h3>
-                    <ul class="basic-list">
-                        <li>Google Chrome <span class="pull-right label-danger label-1 label">95.8%</span></li>
-                        <li>Mozila Firefox <span class="pull-right label-purple label-2 label">85.8%</span></li>
-                        <li>Apple Safari <span class="pull-right label-success label-3 label">23.8%</span></li>
-                        <li>Internet Explorer <span class="pull-right label-info label-4 label">55.8%</span></li>
-                        <li>Opera mini <span class="pull-right label-warning label-5 label">28.8%</span></li>
-                        <li>Mozila Firefox <span class="pull-right label-purple label-6 label">26.8%</span></li>
-                        <li>Safari <span class="pull-right label-purple label-7 label">31.8%</span></li>
-                    </ul>
-                </div>
-            </div>
-
         </div>
     </div>
+</div>
+</div>
 </div>
 
 
